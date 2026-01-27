70e6ed53b47e37426e559966b55eb6711212c47e
rshivakarthik <r.shivakarthik600@gmail.com>
2026-01-28 00:41:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
-import { allBlogs } from 'contentlayer/generated'
-import Main from './Main'
+import Hero from '@/components/portfolio/Hero'
+import Experience from '@/components/portfolio/Experience'
+import Projects from '@/components/portfolio/Projects'
+import CTA from '@/components/portfolio/CTA'
+import About from '@/components/portfolio/About'
 
-export default async function Page() {
-  const sortedPosts = sortPosts(allBlogs)
-  const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+export default function Page() {
+  return (
+    <main className="space-y-32">
+      <Hero />
+      <Experience />
+      <Projects />
+      <CTA />
+      <About />
+    </main>
+  )
 }
